8a1133078329ab39acd69d6d729563b1dc7ca7a1
Roi Martinez <rmartinezroque@danielcastelao.org>
2021-10-06 14:44:15 +0200

    Subidos BOLETIN 1 EXTRA

---
+++ boletin 1 extra/BoletinExtra1_1.java
@@ -0,0 +1,10 @@
+package boletinextra1_1;
+/**
+ * @author dam1
+ */
+public class BoletinExtra1_1 {
+    public static void main(String[] args) {
+        System.out.println("Roi Martínez Roque\n14\\07\\2003");
+    }
+    
+}
