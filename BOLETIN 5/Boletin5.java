ea9d5430eb6077df60a28627ae46f98137787642
Roi Martínez Roque <roimartinezroque@gmail.com>
2021-10-20 13:28:40 +0200

    Actualizado BOLETIN 5

--- BOLETIN 5/Boletin5.java
+++ BOLETIN 5/Boletin5.java
@@ -2,13 +2,25 @@ package boletin5;
 /**
  * @author Roi Martínez
  */
+import java.util.Scanner;
 public class Boletin5 {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
         Consumo coche1 = new Consumo();
         coche1.setLitros(50);
         coche1.setPgas(1.57);
         
-        Consumo coche2 = new Consumo(500f, 90f, 100f, (double)1.38);
+        System.out.println("Introduzca los km recorridos: ");
+        float km = sc.nextFloat();
+        System.out.println("Introduzca los litros consumidos: ");
+        float litros = sc.nextFloat();
+        System.out.println("Introduzca la velocidad media: ");
+        float vMed = sc.nextFloat();
+        System.out.println("Introduzca el precio del carburante: ");
+        double pGal = sc.nextFloat();
+        
+        Consumo coche2 = new Consumo(km, litros, vMed, pGal);
         
         System.out.println("Consumo medio de litros cada 100km del coche2: "+ coche2.consumoMedio());
         
