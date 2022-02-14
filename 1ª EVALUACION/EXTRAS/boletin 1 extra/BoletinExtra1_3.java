f00c54c4a2edefc4e7423f36f1d44f1eb4a3972e
Roi Martinez <rmartinezroque@danielcastelao.org>
2022-02-14 10:56:11 +0100

    Reestructuración de la carpeta EXTRAS 1ªEV

---
+++ 1ª EVALUACION/EXTRAS/boletin 1 extra/BoletinExtra1_3.java
@@ -0,0 +1,15 @@
+package boletinextra1_3;
+/**
+ *
+ * @author dam1
+ */
+public class BoletinExtra1_3 {
+    public static void main(String[] args) {
+        int precio = 90;
+        double descuento = 0.25;
+        double pFinal = precio-(precio*descuento);
+        
+        System.out.println("Precio con descuento : "+pFinal);
+    }
+    
+}
